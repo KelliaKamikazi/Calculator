6e05cca013ce97c20c94690a1a058f87392d7c2e
KelliaKamikazi <kelliakamikazi6@gmail.com>
2024-06-28 12:25:56 +0200

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,80 @@ class CalculatorApp extends StatelessWidget {
   }
 }
 
-class CalculatorScreen extends StatelessWidget {
+class CalculatorScreen extends StatefulWidget {
   const CalculatorScreen({super.key});
+
+  @override
+  _CalculatorScreenState createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  String _display = '0';
+  String _operand = '';
+  double _firstOperand = 0;
+  double _secondOperand = 0;
+
+  void _onButtonPressed(String buttonText) {
+    setState(() {
+      if (buttonText == 'C') {
+        _clear();
+      } else if (buttonText == '=' && _operand.isNotEmpty) {
+        _calculateResult();
+      } else if ('+-x/'.contains(buttonText)) {
+        _setOperand(buttonText);
+      } else {
+        _updateDisplay(buttonText);
+      }
+    });
+  }
+
+  void _clear() {
+    _display = '0';
+    _operand = '';
+    _firstOperand = 0;
+    _secondOperand = 0;
+  }
+
+  void _setOperand(String operand) {
+    if (_operand.isEmpty) {
+      _firstOperand = double.tryParse(_display) ?? 0;
+      _operand = operand;
+      _display = '0';
+    }
+  }
+
+  void _updateDisplay(String text) {
+    if (_display == '0') {
+      _display = text;
+    } else {
+      _display += text;
+    }
+  }
+
+  void _calculateResult() {
+    _secondOperand = double.tryParse(_display) ?? 0;
+    double result = 0;
+
+    switch (_operand) {
+      case '+':
+        result = _firstOperand + _secondOperand;
+        break;
+      case '-':
+        result = _firstOperand - _secondOperand;
+        break;
+      case 'x':
+        result = _firstOperand * _secondOperand;
+        break;
+      case '/':
+        result = _firstOperand / _secondOperand;
+        break;
+    }
+
+    _display = result.toString();
+    _operand = '';
+    _firstOperand = result;
+    _secondOperand = 0;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,9 +108,9 @@ class CalculatorScreen extends StatelessWidget {
             child: Container(
               padding: const EdgeInsets.all(16),
               alignment: Alignment.bottomRight,
-              child: const Text(
-                '0',
-                style: TextStyle(
+              child: Text(
+                _display,
+                style: const TextStyle(
                   fontSize: 48,
                   fontWeight: FontWeight.bold,
                 ),
@@ -72,21 +144,19 @@ class CalculatorScreen extends StatelessWidget {
       child: Container(
         margin: const EdgeInsets.all(8),
         child: ElevatedButton(
-          onPressed: () {},
+          onPressed: () => _onButtonPressed(text),
           style: ElevatedButton.styleFrom(
             padding: const EdgeInsets.all(24),
             shape: const CircleBorder(),
-            backgroundColor: _getButtonColor(text), // Updated here
+            backgroundColor: _getButtonColor(text),
           ),
           child: Text(
             text,
             style: const TextStyle(
               fontSize: 24,
               fontWeight: FontWeight.bold,
-            
             ),
           ),
-          
         ),
       ),
     );
